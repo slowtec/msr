175d6c0ee7710d2c347f813fdd7d2a6f0c063c5a
Markus Kohlhase <mail@markus-kohlhase.de>
2018-06-14 18:16:07 +0200

    simplify Input/Output structs

--- src/entities.rs
+++ src/entities.rs
@@ -6,81 +6,53 @@ use super::*;
 /// ```rust,no_run
 /// use msr::*;
 ///
-/// let tcr001 = Input::new("tcr001");
+/// let tcr001 = Input::new("tcr001".into());
 ///
 /// // or create it from a str
 /// let tcr002 : Input = "tcr002".into();
 ///
 /// // You can also add some description to it
-/// let mut tcr003 = Input::new("tcr003");
-/// tcr003.desc = Some("This sensor measures the environment temperature");
+/// let mut tcr003 = Input::new("tcr003".into());
+/// tcr003.desc = Some("This sensor measures the environment temperature".into());
 /// ```
-#[derive(Debug, Clone, Copy, Eq)]
-pub struct Input<'a> {
+#[derive(Debug, Clone, PartialEq)]
+pub struct Input {
     /// The unique ID of the input
-    pub id: &'a str,
+    pub id: String,
     /// A more detailed description
-    pub desc: Option<&'a str>,
+    pub desc: Option<String>,
 }
 
-impl<'a> Input<'a> {
-    pub fn new(id: &'a str) -> Self {
+impl Input {
+    pub fn new(id: String) -> Self {
         Input { id, desc: None }
     }
 }
 
-impl<'a> From<&'a str> for Input<'a> {
+impl<'a> From<&'a str> for Input {
     fn from(id: &'a str) -> Self {
-        Input::new(id)
-    }
-}
-
-impl<'a> PartialEq for Input<'a> {
-    fn eq(&self, other: &Input) -> bool {
-        self.id == other.id
-    }
-}
-
-use std::hash::{Hash, Hasher};
-
-impl<'a> Hash for Input<'a> {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.id.hash(state);
-        "in".hash(state);
+        Input::new(id.into())
     }
 }
 
 /// Output gate (actuator)
-#[derive(Debug, Clone, Copy, Eq)]
-pub struct Output<'a> {
+#[derive(Debug, Clone, PartialEq)]
+pub struct Output {
     /// The unique ID of the output
-    pub id: &'a str,
+    pub id: String,
     /// A more detailed description
-    pub desc: Option<&'a str>,
+    pub desc: Option<String>,
 }
 
-impl<'a> Output<'a> {
-    pub fn new(id: &'a str) -> Self {
+impl Output {
+    pub fn new(id: String) -> Self {
         Output { id, desc: None }
     }
 }
 
-impl<'a> From<&'a str> for Output<'a> {
+impl<'a> From<&'a str> for Output {
     fn from(id: &'a str) -> Self {
-        Output::new(id)
-    }
-}
-
-impl<'a> PartialEq for Output<'a> {
-    fn eq(&self, other: &Output) -> bool {
-        self.id == other.id
-    }
-}
-
-impl<'a> Hash for Output<'a> {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.id.hash(state);
-        "out".hash(state);
+        Output::new(id.into())
     }
 }
 
@@ -95,89 +67,4 @@ pub struct Rule {
     pub condition: BooleanExpr<Comparison>,
     /// Actions that should be triggerd
     pub actions: Vec<String>,
-}
-
-#[cfg(test)]
-mod tests {
-
-    use super::*;
-    use std::collections::hash_map::DefaultHasher;
-    use std::hash::Hasher;
-
-    #[test]
-    fn input_equality() {
-        let a0 = Input {
-            id: "a",
-            desc: None,
-        };
-        let a1 = Input {
-            id: "a",
-            desc: Some("foo"),
-        };
-        assert!(a0.eq(&a1));
-    }
-
-    #[test]
-    fn output_equality() {
-        let o0 = Output {
-            id: "a",
-            desc: None,
-        };
-        let o1 = Output {
-            id: "a",
-            desc: Some("foo"),
-        };
-        assert!(o0.eq(&o1));
-    }
-
-    #[test]
-    fn input_hash() {
-        let a0 = Input {
-            id: "a",
-            desc: None,
-        };
-        let a1 = Input {
-            id: "a",
-            desc: Some("foo"),
-        };
-        let mut h0 = DefaultHasher::new();
-        let mut h1 = DefaultHasher::new();
-        a0.hash(&mut h0);
-        a1.hash(&mut h1);
-        assert_eq!(h0.finish(), h1.finish());
-    }
-
-    #[test]
-    fn output_hash() {
-        let o0 = Output {
-            id: "a",
-            desc: None,
-        };
-        let o1 = Output {
-            id: "a",
-            desc: Some("foo"),
-        };
-        let mut h0 = DefaultHasher::new();
-        let mut h1 = DefaultHasher::new();
-        o0.hash(&mut h0);
-        o1.hash(&mut h1);
-        assert_eq!(h0.finish(), h1.finish());
-    }
-
-    #[test]
-    fn in_and_output_hash() {
-        let o = Output {
-            id: "a",
-            desc: None,
-        };
-        let i = Input {
-            id: "a",
-            desc: None,
-        };
-        let mut h0 = DefaultHasher::new();
-        let mut h1 = DefaultHasher::new();
-        o.hash(&mut h0);
-        i.hash(&mut h1);
-        assert!(h0.finish() != h1.finish());
-    }
 }
