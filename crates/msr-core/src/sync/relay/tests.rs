09fc0054ead561ae2598c6f10eb6ab646130c8de
Uwe Klotz <uwe.klotz@slowtec.de>
2021-11-08 21:07:22 +0100

    sync::Relay: Add tests for wait_until() with an expired deadline

--- crates/msr-core/src/sync/relay/tests.rs
+++ crates/msr-core/src/sync/relay/tests.rs
@@ -1,4 +1,4 @@
-use std::time::Duration;
+use std::time::{Duration, Instant};
 
 type Relay = super::Relay<()>;
 
@@ -29,6 +29,26 @@ fn wait_for_timeout_max_ready() -> anyhow::Result<()> {
     relay.replace_notify_one(());
 
     assert!(relay.wait_for(Duration::MAX).is_some());
+
+    Ok(())
+}
+
+#[test]
+fn wait_until_deadline_now_empty() -> anyhow::Result<()> {
+    let relay = Relay::default();
+
+    assert!(relay.wait_until(Instant::now()).is_none());
+
+    Ok(())
+}
+
+#[test]
+fn wait_until_deadline_now_ready() -> anyhow::Result<()> {
+    let relay = Relay::default();
+
+    relay.replace_notify_one(());
+
+    assert!(relay.wait_until(Instant::now()).is_some());
 
     Ok(())
 }
